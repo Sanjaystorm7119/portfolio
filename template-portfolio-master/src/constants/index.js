bc3fbcf12055e6bd20eb9365b7de38ca80016636
Sanjay b <sanjaysrivastavab@gmail.com>
2025-09-13 22:50:49 -0700

    first commit

---
+++ template-portfolio-master/src/constants/index.js
@@ -0,0 +1,178 @@
+// Make changes here!!
+
+export const theme = "#ff00ff"; // red
+
+export const name = "Sanjay B";
+
+export const aboutMe = "Let's Build break fix Repeat ...";
+
+export const education1 = {
+  name: "Saveetha Engineering College",
+  branch: "B.Tech - Information Technology.",
+  year: "2016 - 2020",
+};
+
+export const education2 = {
+  // name: "Lorem ipsum dolor sit amet.",
+  // branch: "Lorem ipsum dolor sit amet.",
+  // year: "20XX - 20XX",
+};
+
+// export const experience1 = {
+//   name: "Lorem ipsum dolor sit amet.",
+//   moreInfo: "Lorem ipsum dolor sit amet.",
+//   year: "Month 20XX - Month 20XX",
+// };
+export const experience = [
+  {
+    name: "HireAva by StelleHire",
+    moreInfo: "Full stack dev",
+    year: "Apr 2025 - Present",
+  },
+  {
+    name: "TVS credit",
+    moreInfo: "Software developer",
+    year: "Jun 2023 - Apr 2024",
+  },
+  {
+    name: "stellehire",
+    moreInfo: "frontend developer",
+    year: "Apr 2021 - May 2023",
+  },
+];
+
+export const projects = [
+  {
+    name: "",
+    description: "",
+
+    href: "/",
+    cta: "Learn more",
+    background: "/images/wall4.jpg",
+    className:
+      "md:col-start-1 md:col-end-4 md:row-start-3 md:row-end-5 col-start-2 col-end-3 row-start-1 row-end-4 ",
+  },
+  {
+    name: "",
+    description: "",
+
+    href: "/",
+    cta: "Learn more",
+    background: "/images/gg.jpg",
+    className:
+      "md:row-start-1 md:row-end-3 md:col-start-3 md:col-end-5 col-start-1 col-end-2 row-start-1 row-end-3",
+  },
+  {
+    name: "",
+    description: "",
+
+    href: "/",
+    cta: "Learn more",
+    background: "/images/gg4.jpg",
+    className:
+      "md:col-start-1 md:col-end-3 md:row-start-1 md:row-end-2  col-start-1 col-end-3 row-start-5 row-end-6 ",
+  },
+  {
+    name: "",
+    description: "",
+    // "Stack: Nextjs, tailwindcss,Openai, Vapi,Supabase",
+
+    href: "/",
+    cta: "Learn more",
+    background: "/images/wall.jpg",
+
+    className:
+      "md:col-start-4 md:col-end-5 md:row-start-3 md:row-end-5 col-start-1 col-end-3 row-start-4 row-end-5",
+  },
+  {
+    name: "",
+    description: "",
+
+    href: "/",
+    cta: "Learn more",
+    background: "/images/wall5.jpg",
+    className:
+      " md:col-start-1 md:col-end-3 md:row-start-2 md:row-end-3  col-start-1 col-end-2 row-start-3 row-end-3 ",
+  },
+];
+
+// change fill color of icons manually
+export const skills = [
+  {
+    title: "Reactjs",
+    src: "/icons/1.svg",
+  },
+  {
+    title: "NodeJs",
+    src: "/icons/2.svg",
+  },
+  {
+    title: "Npm",
+    src: "/icons/3.svg",
+  },
+  {
+    title: "Nextjs",
+    src: "/icons/4.svg",
+  },
+  {
+    title: "Fastapi",
+    src: "/icons/20.svg",
+  },
+  {
+    title: "Expressjs",
+    src: "/icons/6.svg",
+  },
+  {
+    title: "MongoDB",
+    src: "/icons/7.svg",
+  },
+  {
+    title: "Vercel",
+    src: "/icons/8.svg",
+  },
+  {
+    title: "Python",
+    src: "/icons/19.svg",
+  },
+  {
+    title: "Javascript",
+    src: "/icons/10.svg",
+  },
+  {
+    title: "Typescript",
+    src: "/icons/11.svg",
+  },
+  {
+    title: "Html",
+    src: "/icons/12.svg",
+  },
+  {
+    title: "Css",
+    src: "/icons/13.svg",
+  },
+  {
+    title: "Bootstrap",
+    src: "/icons/14.svg",
+  },
+  {
+    title: "Tailwind",
+    src: "/icons/15.svg",
+  },
+  {
+    title: "Github",
+    src: "/icons/16.svg",
+  },
+  {
+    title: "SQL",
+    src: "/icons/17.svg",
+  },
+  {
+    title: "Docker",
+    src: "/icons/18.svg",
+  },
+];
+
+export const socials = {
+  github: "https://github.com/Sanjaystorm7119",
+  LinkedIn: "https://www.linkedin.com/in/sanjaysrivastavab/",
+};
